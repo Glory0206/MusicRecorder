98ca7f3aa2be513fafe6791538738ebcaf25ef6b
Yeonggwang <dmb07223@gmail.com>
2025-07-27 15:25:51 +0900

    feat: signup & login

---
+++ backend/src/test/java/com/mr/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
